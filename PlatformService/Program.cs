af7335d5a9cac19234884e40df7c5ec488fc4c39
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-02-07 19:55:25 +0100

    Add data repo & db context

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,3 +1,5 @@
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase);
 
 var app = builder.Build();
 
