e7fd364e26786cd70136f18561db71d34672273f
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-02-11 16:19:42 +0100

    Implemented tester

--- PlatformService.Tester/Program.cs
+++ PlatformService.Tester/Program.cs
@@ -6,28 +6,28 @@ using PlatformService.Models;
 Console.WriteLine("Test cosmos sdk v3!");
 
 var endpoint = "<uri>";
-var key = "<key>";
+var key = "<ke>";
 
-CosmosClientBuilder cosmosClientBuilder = new CosmosClientBuilder(accountEndpoint: endpoint, authKeyOrResourceToken: key)
-    .WithConsistencyLevel(ConsistencyLevel.Session)
-    .WithApplicationRegion(Regions.SwitzerlandNorth);
+CosmosClientBuilder cosmosClientBuilder = new CosmosClientBuilder(accountEndpoint: endpoint, authKeyOrResourceToken: key);
+    //.WithConsistencyLevel(ConsistencyLevel.Session)
+    //.WithApplicationRegion(Regions.SwitzerlandNorth);
 
 CosmosClient client = cosmosClientBuilder.Build();
 
 var id = "c5151d13-a2ae-4582-8db9-8ee8655c5fe5";
-var database = client.GetDatabase("astraeus-dev");
-var container = client.GetContainer("astraeus-dev", "platforms");
+var databaseName = "astraeus-dev";
+var containerName = "platforms";
+//var database = client.GetDatabase("astraeus-dev");
 
 Platform? p = null;
 
-// repo
-var repo = new PlatformRepository(client, "astraeus-dev", "platforms");
+// read direct
+var container = client.GetContainer(databaseName, containerName);
+ItemResponse<Platform> response = await container.ReadItemAsync<Platform>(id, new PartitionKey(id));
+p = response;
+Console.WriteLine($"Platform from client {p.Name} ({p.Id})");
 
-// read something direct
-//ItemResponse<Platform> response = await container.ReadItemAsync<Platform>(id, new PartitionKey(id));
-//p = response;
-
-// red from repo
-p = await repo.GetItemByIdAsync(id);
-
-Console.WriteLine($"Platform {p.Name} ({p.Id})");
+// read from repo
+var repository = new PlatformRepository(client, databaseName, containerName);
+p = await repository.GetItemByIdAsync(id);
+Console.WriteLine($"Platform from repo {p.Name} ({p.Id})");
