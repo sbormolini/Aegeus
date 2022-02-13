84b04bebaa1e8c62b3dc2880ba87f88e82827e2d
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-02-13 22:44:43 +0100

    Add Base entities

---
+++ PlatformService/Data/Base/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+
+namespace PlatformService.Data.Base.Entities;
+
+public abstract class BaseEntity
+{
+    [JsonProperty(PropertyName = "id")]
+    public virtual string Id { get; set; }
+}
---
+++ PlatformService/Data/Base/Entities/Platform.cs
@@ -0,0 +1,10 @@
+﻿namespace PlatformService.Data.Base.Entities;
+
+public class Platform : BaseEntity
+{
+    public string? Name { get; set; }
+
+    public string? Publisher { get; set; }
+
+    public string? Cost { get; set; }
+}
